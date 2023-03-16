c780f2577a36b4abe2ee3a74c5424e297dbead2b
Amadaro <amadarog@gmail.com>
2023-03-16 19:40:17 +0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ function Header(props) {
 
   return (
     <header className="d-flex justify-between align-center p-40">
-      <Link to={process.env.PUBLIC_URL + '/'}>
+      <Link to="/">
         <div className="d-flex align-center">
           <img width={40} height={40} src="img/logo.png" alt="Logo" />{' '}
           <div>
@@ -22,12 +22,12 @@ function Header(props) {
           <span>{totalPrice} р.</span>
         </li>
         <li className="mr-20 cu-p">
-          <Link to={process.env.PUBLIC_URL + '/favorites'}>
+          <Link to="/favorites">
             <img width={18} height={18} src="img/heart.svg" alt="Закладки" />
           </Link>
         </li>
         <li>
-          <Link to={process.env.PUBLIC_URL + '/orders'}>
+          <Link to="/orders">
             <img width={18} height={18} src="img/user.svg" alt="Пользователь" />
           </Link>
         </li>
